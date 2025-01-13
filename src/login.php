2f14b546d221eafad46faa1098250feb388ae08a
Ruan <ruanlimafn@gmail.com>
2025-01-13 19:19:20 -0300

    Melhoria no sistema de verificação -- Combate a SQL Injections através de prepared statements

--- src/login.php
+++ src/login.php
@@ -1,37 +1,46 @@
 <?php
-session_start(); // Inicia a sessão
+session_start(); // Start the session
 
-// Inclui o arquivo de configuração da conexão com o banco
+// Include our conection file
 include 'config.php';
 
-function query($query, $conn){
-    $sql_query = $conn->query($query);
-    return $sql_query;
+// Function to execute prepared statements with parameters (query parameters)
+function query($query, $params, $conn){
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param(...$params);
+    $stmt->execute();
+    return $stmt->get_result();
 }
 
-$email = $conn->real_escape_string($_POST['email']);
-$password = $conn->real_escape_string($_POST['password']);
+// Get form data from POST request
+$email = $_POST['email'];
+$password = $_POST['password'];
 
-$sql_busca_email = query("SELECT * FROM person WHERE login_email = '$email'", $conn);
+// Check if user exists in the database by querying the login_email column
+$sql_busca_email = query("SELECT * FROM person WHERE login_email = ?",['s', $email] ,$conn);
 
+// If user exists, check if password is correct
 if ($sql_busca_email->num_rows > 0) {
     echo "Usuário encontrado: " . $email;
 
-
-
-    $sql_busca_password = query("SELECT * FROM person WHERE login_email = '$email' AND login_password = '$password'", $conn);
+    $sql_busca_password = query("SELECT * FROM person WHERE login_email = ? AND login_password = ?", ['ss', $email, $password] ,$conn);
     if ($sql_busca_password->num_rows>0) {
-        echo "usuario autenticado";
+        // Login successful,  redirect to dashboard page
         header("Location: dashboard.html");
+        exit();
     
     } else {
+        // Incorrect password, display error message and continue in the login page
         $_SESSION['error'] = 'Senha incorreta';
         header("Location: index.php");
+        exit();
     }
     
 
 } else {
+    // User not found, display error message and continue in the login page
     $_SESSION['error'] = "Usuário não encontrado";
     header("Location: index.php");
+    exit();
 }
 ?>
