83f1c897eb1438954698d4847f0bc9267d14fcc4
Ruan <ruanlimafn@gmail.com>
2025-01-18 00:07:11 -0300

    Conclusão do processo de validação da criação de conta

--- src/JS/validate.js
+++ src/JS/validate.js
@@ -1,34 +1,193 @@
+function validate_password() {
+    const password = document.getElementById("password");
 
+    const length = document.getElementById("length");
+    const number = document.getElementById("number");
+    const letter = document.getElementById("letter");
+    const special = document.getElementById("special");
+    let validate = false;
+    var i=0;
+    console.log("validate_password");
+
+    // first condition
+    if(password.value.length >= 8){
+        length.classList.remove("text-red-500");
+        length.classList.add("text-green-500");
+        length.classList.remove("marker:text-red-500");
+        length.classList.add("marker:text-green-500");
+        i++;
+    }
+    else{
+        length.classList.remove("text-green-500");
+        length.classList.add("text-red-500");
+        length.classList.remove("marker:text-green-500");
+        length.classList.add("marker:text-red-500");
+        i--;
+    }
+
+    // second condition  
+    if(/\d/.test(password.value)){
+        number.classList.remove("text-red-500");
+        number.classList.add("text-green-500");
+        number.classList.remove("marker:text-red-500");
+        number.classList.add("marker:text-green-500");
+        i++;
+    }
+    else{
+        number.classList.remove("text-green-500");
+        number.classList.add("text-red-500");
+        number.classList.remove("marker:text-green-500");
+        number.classList.add("marker:text-red-500");
+        i--;
+    }
+
+    // third condition
+    if(/[a-zA-Z]/.test(password.value)){
+        letter.classList.remove("text-red-500");
+        letter.classList.add("text-green-500");
+        letter.classList.remove("marker:text-red-500");
+        letter.classList.add("marker:text-green-500");
+        i++;
+    }
+    else{
+        letter.classList.remove("text-green-500");
+        letter.classList.add("text-red-500");
+        letter.classList.remove("marker:text-green-500");
+        letter.classList.add("marker:text-red-500");
+    }
+
+
+    // fourth condition
+    if(/[!@#$%^&*¨(),.?":{}=|<>;:_+-]/.test(password.value)){
+        special.classList.remove("text-red-500");
+        special.classList.add("text-green-500");
+        special.classList.remove("marker:text-red-500");
+        special.classList.add("marker:text-green-500");
+        i++;
+    }
+    else{
+        special.classList.remove("text-green-500");
+        special.classList.add("text-red-500");
+        special.classList.remove("marker:text-green-500");
+        special.classList.add("marker:text-red-500");
+        i--;
+    }
+
+    if(i==4){
+        validate = true;
+        console.log("1");
+        password.classList.remove("focus:ring-sky-500");
+        password.classList.remove("focus:ring-red-500");
+        password.classList.add("focus:ring-green-500");
+        password.classList.remove("border-slate-300");
+        password.classList.remove("border-red-500");
+        password.classList.add("border-green-500");
+    }
+    else{
+        validate = false;
+        password.classList.remove("border-slate-300");
+        password.classList.remove("border-green-500");
+        password.classList.add("border-red-500");
+        password.classList.add("focus:ring-red-500");
+        password.classList.remove("focus:ring-sky-500");
+        password.classList.remove("focus:ring-green-500");
+    }
+    if(password.value == ""){
+        password.classList.add("border-slate-300");
+        password.classList.remove("border-green-500");
+        password.classList.remove("border-red-500");
+        password.classList.remove("focus:ring-red-500");
+        password.classList.add("focus:ring-sky-500");
+        password.classList.remove("focus:ring-green-500");
+    }
+    
+    
+    return validate;
+}
+
+function validate_username(){
+    const username = document.getElementById("username");
+    let validate = false;
+
+    if(username.validity.valid){
+        validate = true;
+        username.classList.remove("border-slate-300");
+        username.classList.remove("border-red-500");
+        username.classList.remove("focus:ring-red-500");
+        username.classList.add("focus:ring-sky-500");
+    }
+    else{
+        validate = false;
+        username.classList.remove("border-slate-300");
+        username.classList.add("border-red-500");
+        username.classList.remove("focus:ring-sky-500");
+        username.classList.add("focus:ring-red-500");
+
+    }
+
+    return validate; 
+}
+
+// procurar um jeito melhor de realizar essa abordagem, usando o async=true
 function validate_email() {
-    const email = $('#email') // Pega o valor atualizado do email com jQuery
+    const email = $('#email');
     const alert_email = $('#alert_email');
-    
-    /// const alert_email = $("#alert_email"); // Pega o elemento de alerta com jQuery
-    
-    // Exibir no console toda vez que o usuário digitar
-    console.log(email);
-    
-    $.post("verify_email.php", {email: email.val()})
-        .done(function(data){
-            console.log("1");
-            if(data == "Email is already in use"){
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    let validate = false;
+
+    $.ajax({
+        url: "verify_email.php",
+        type: "POST",
+        data: { email: email.val() },
+        async: false, 
+        success: function(data) {
+            if (data === "Email is already in use") {
+                validate = false;
                 alert_email.css("display", "block");
                 email.removeClass("focus:ring-sky-500").addClass("focus:ring-red-500");
                 email.removeClass("border-slate-300").addClass("border-red-500");
-                console.log("2");
-            }
-        
-            
-            else{
+                console.log("Email já está em uso");
+            } else {
+                validate = true;
                 alert_email.css("display", "none");
                 email.removeClass("focus:ring-red-500").addClass("focus:ring-sky-500");
                 email.removeClass("border-red-500").addClass("border-slate-300");
-                console.log("3");
+                console.log("Email disponível");
             }
-            
-        });
+        },
+        error: function(xhr, status, error) {
+            console.error("Erro na requisição AJAX:", error);
+        }
+    });
+
+    
+
+    return validate;  // Agora esse retorno vai ser feito após a requisição
+}
+
+function validate_email2(){
+    const email = document.getElementById("email");
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+    if (emailRegex.test(email.value)){
+        if (email.value.length > 0){
+            email.classList.remove("border-red-500", "focus:ring-red-500");
+            email.classList.add("border-slate-300", "focus:ring-sky-500");
+            return true;
+        }
+        
+    }
+    else{
+        if (email.value.length > 0){
+            email.classList.add("border-red-500", "focus:ring-red-500");
+            email.classList.remove("border-slate-300", "focus:ring-sky-500");
+            return false;
+        }
+        
+    }
 
 }
+
 
 function validate_form(){
     const submit = document.getElementById("submit");
@@ -36,19 +195,22 @@ function validate_form(){
     const email = document.getElementById("email");
     validate_to_match = validate_match();
     validate_to_password = validate_password();
+    validate_to_email = validate_email();
+    validate_to_email2 = validate_email2();
+    validate_to_username = validate_username();
 
-    if (validate_to_password && validate_to_match){
-   
-        
-    }
-    else{
-        event.preventDefault();
-        alert.style.display = "block"; 
-        setTimeout(() => {
-        if (alert) alert.style.display = 'none';
-        }, 3000);
-        
-        
+    console.log(validate_to_email, validate_to_email2, validate_to_match, validate_to_password, validate_to_username);
+
+    if (validate_to_password && validate_to_match && validate_to_email && validate_to_email2 && validate_to_username) {
+        submit.disabled = false;
+        submit.classList.add("bg-indigo-500", "text-gray-100", "transition", "ease-in-out", "delay-75", "hover:-translate-y-1", "hover:scale-110", "hover:bg-indigo-600");
+        submit.classList.remove("bg-slate-300", "text-gray-400");
+        console.log("FALSO");
+    } else {
+        submit.disabled = true;
+        submit.classList.add("bg-slate-300", "text-gray-400");
+        submit.classList.remove("bg-indigo-500", "text-gray-100", "transition", "ease-in-out", "delay-75", "hover:-translate-y-1", "hover:scale-110", "hover:bg-indigo-600");
+        console.log("VERDADEIRO");
     }
     
     
@@ -60,7 +222,7 @@ function validate_match(){
     const match = document.getElementById("match");
     const submit = document.getElementById("submit");
     let validate = false;
-    
+    console.log("MACTCH");
     
     if(password.value != "" && password2.value != ""){
         // If password do not match
@@ -104,91 +266,7 @@ function validate_match(){
 
 }
 
-function validate_password() {
-    const password = document.getElementById("password").value;
 
-    const length = document.getElementById("length");
-    const number = document.getElementById("number");
-    const letter = document.getElementById("letter");
-    const special = document.getElementById("special");
-    const list = document.getElementById("list");
-    let validate = false;
-    let i=0;
-    
-    // first condition
-    if(password.length >= 8){
-        length.classList.remove("text-red-500");
-        length.classList.add("text-green-500");
-        length.classList.remove("marker:text-red-500");
-        length.classList.add("marker:text-green-500");
-        i++;
-    }
-    else{
-        length.classList.remove("text-green-500");
-        length.classList.add("text-red-500");
-        length.classList.remove("marker:text-green-500");
-        length.classList.add("marker:text-red-500");
-        i--;
-    }
-
-    // second condition  
-    if(/\d/.test(password)){
-        number.classList.remove("text-red-500");
-        number.classList.add("text-green-500");
-        number.classList.remove("marker:text-red-500");
-        number.classList.add("marker:text-green-500");
-        i++;
-    }
-    else{
-        number.classList.remove("text-green-500");
-        number.classList.add("text-red-500");
-        number.classList.remove("marker:text-green-500");
-        number.classList.add("marker:text-red-500");
-        i--;
-    }
-
-    // third condition
-    if(/[a-zA-Z]/.test(password)){
-        letter.classList.remove("text-red-500");
-        letter.classList.add("text-green-500");
-        letter.classList.remove("marker:text-red-500");
-        letter.classList.add("marker:text-green-500");
-        i++;
-    }
-    else{
-        letter.classList.remove("text-green-500");
-        letter.classList.add("text-red-500");
-        letter.classList.remove("marker:text-green-500");
-        letter.classList.add("marker:text-red-500");
-    }
-
-
-    // fourth condition
-    if(/[!@#$%^&*¨(),.?":{}=|<>;:_+-]/.test(password)){
-        special.classList.remove("text-red-500");
-        special.classList.add("text-green-500");
-        special.classList.remove("marker:text-red-500");
-        special.classList.add("marker:text-green-500");
-        i++;
-    }
-    else{
-        special.classList.remove("text-green-500");
-        special.classList.add("text-red-500");
-        special.classList.remove("marker:text-green-500");
-        special.classList.add("marker:text-red-500");
-        i--;
-    }
-    
-    if(i==4){
-        validate = true;
-
-    }
-    else{
-        validate = false;
-    }
-    
-    return validate;
-}
 
 
 
--- src/create.php
+++ src/create.php
@@ -37,7 +37,7 @@
                 <div class="">
                     <label for="text" class="block"> 
                         <span class="after:content-['*'] after:ml-0.5 after:text-red-500 font-medium text-slate-700 text-sm">Name</span>
-                        <input type="text" name="username" placeholder="Enter your full name" required minlength="2" class="bg-white border shadow-sm border-slate-300 
+                        <input type="text" name="username" id="username" placeholder="Enter your full name" oninput="validate_form(); validate_username()" required minlength="2" class="bg-white border shadow-sm border-slate-300 
                         placeholder-slate-400 placeholder:font-normal text-slate-600 font-light focus:outline-none focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 
                         p-3 py-2 ">
                     </label>
@@ -48,18 +48,18 @@
                 <div>
                     <label for="email" class="block"> 
                         <span class="after:content-['*'] after:ml-0.5 after:text-red-500 font-medium text-slate-700 text-sm">Email</span>
-                        <input type="email" name="email" id="email" placeholder="you@example.com" oninput="validate_email()" required class="bg-white border shadow-sm border-slate-300 
+                        <input type="email" name="email" id="email" placeholder="you@example.com" oninput="validate_email(); validate_email2(); validate_form()" required class="bg-white border shadow-sm border-slate-300 
                         placeholder-slate-400 placeholder:font-normal text-slate-600 font-light focus:outline-none focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 
                         p-3 py-2 ">
                     </label>
-                    <span id="alert_email" style="display: none" class=" text-xs text-red-500 font-light italic">This email is already in use</span>
+                    <span id="alert_email" style="display: none" class=" text-sm text-red-500 font-light">This email is already in use</span>
                 </div>
                 <br>
                 <br>
                 <div>
                     <label for="password" class="block"> 
                         <span class="after:content-['*'] after:ml-0.5 after:text-red-500 font-medium text-slate-700 text-sm">Password</span>
-                        <input type="password" name="password" id="password" oninput="validate_password(); validate_match()" placeholder="Enter your password" required class="bg-white border shadow-sm border-slate-300 
+                        <input type="password" name="password" id="password" oninput="validate_password(); validate_match(); validate_form()" placeholder="Enter your password" required class="bg-white border shadow-sm border-slate-300 
                         placeholder-slate-400 placeholder:font-normal text-slate-600 font-light focus:outline-none focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 
                         p-3 py-2">
                     </label>
@@ -78,19 +78,17 @@
                     
                 <div>
                     <label for="password" class="block"> 
-                        <input type="password" name="password2" id="password2" oninput="validate_match()" placeholder="Enter your password again" required class="bg-white border shadow-sm border-slate-300 
+                        <input type="password" name="password2" id="password2" oninput="validate_match(); validate_form()" placeholder="Enter your password again" required class="bg-white border shadow-sm border-slate-300 
                         placeholder-slate-400 placeholder:font-normal text-slate-600 font-light focus:outline-none focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 
                         p-3 py-2 mt-4">
                     </label>
-                    <span id="match" class="font-light italic text-sm"></span>
+                    <span id="match" class="font-light text-sm"></span>
                 
                 </div>
                 <br><br>
                 <br><br>
                 <div class="flex items-center place-content-center">
-                    <button type="submit" name="submit" id="submit" onclick="validate_form()" class="bg-indigo-500 rounded-md font-bold text-gray-100 transition ease-in-out delay-75 hover:-translate-y-1 
-                    hover:scale-110 hover:bg-indigo-600
-                    px-24 py-2 m-7 shadow-md">Create</button>
+                    <button type="submit" disabled="true" name="submit" id="submit" onclick="validate_form()" class=" bg-slate-300 text-gray-400 rounded-md font-bold  px-24 py-2 m-7 shadow-md">Create</button>
                 </div>
                 <div>
 
@@ -98,172 +96,8 @@
                     
                 <script src="./JS/Jquery/jquery-3.7.1.min.js"></script>
                 <script src="./JS/validate.js"></script>
-                <script>
-
-                    
-
-                    
-                    function validate_form(){
-                        const submit = document.getElementById("submit");
-                        const alert = document.getElementById("alert");
-                        const email = document.getElementById("email");
-                        validate_to_match = validate_match();
-                        validate_to_password = validate_password();
-                    
-                        if (validate_to_password && validate_to_match){
-                       
-                            
-                        }
-                        else{
-                            event.preventDefault();
-                            alert.style.display = "block"; 
-                            setTimeout(() => {
-                            if (alert) alert.style.display = 'none';
-                            }, 3000);
-                            
-                            
-                        }
-                        
-                        
-                    }
-
-                    function validate_match(){
-                        const password = document.getElementById("password");
-                        const password2 = document.getElementById("password2");
-                        const match = document.getElementById("match");
-                        const submit = document.getElementById("submit");
-                        let validate = false;
-                        
-                        
-                        if(password.value != "" && password2.value != ""){
-                            // If password do not match
-                            if (password.value != password2.value){
-                                match.textContent = "Passwords do not match";
-                                password2.classList.remove("focus:ring-sky-500");
-                                password2.classList.remove("focus:ring-green-500");
-                                password2.classList.remove("border-slate-500");
-                                password2.classList.remove("border-green-500")
-                                match.classList.remove("text-green-500");
-                                match.classList.add("text-red-500");
-                                password2.classList.add("border-red-500");
-                                password2.classList.add("focus:ring-red-500");
-                                validate = false;
-                            }
-                            // If passwords match
-                            if (password.value == password2.value){
-                                match.textContent = "Passwords match";
-                                match.classList.remove("text-red-500");
-                                password2.classList.remove("border-red-500");
-                                password2.classList.remove("focus:ring-red-500");
-                                match.classList.add("text-green-500");
-                                password2.classList.add("border-green-500");
-                                password2.classList.add("focus:ring-green-500");
-                                validate = true;
-                            }
-                        }
-                        if(password2.value == ""){
-                            match.textContent = "";
-                            password2.classList.remove("border-green-500");
-                            password2.classList.remove("border-red-500");
-                            password2.classList.remove("focus:ring-green-500");
-                            password2.classList.remove("focus:ring-red-500");
-                            password2.classList.add("border-slate-500");
-                            password2.classList.add("focus:ring-sky-500");
-                            validate = false;
-                        }
-                        
-                        return validate;
-                        
-
-                    }
-
-
-                    function validate_password() {
-                        const password = document.getElementById("password").value;
-
-                        const length = document.getElementById("length");
-                        const number = document.getElementById("number");
-                        const letter = document.getElementById("letter");
-                        const special = document.getElementById("special");
-                        const list = document.getElementById("list");
-                        let validate = false;
-                        let i=0;
-                        
-                        // first condition
-                        if(password.length >= 8){
-                            length.classList.remove("text-red-500");
-                            length.classList.add("text-green-500");
-                            length.classList.remove("marker:text-red-500");
-                            length.classList.add("marker:text-green-500");
-                            i++;
-                        }
-                        else{
-                            length.classList.remove("text-green-500");
-                            length.classList.add("text-red-500");
-                            length.classList.remove("marker:text-green-500");
-                            length.classList.add("marker:text-red-500");
-                            i--;
-                        }
-
-                        // second condition  
-                        if(/\d/.test(password)){
-                            number.classList.remove("text-red-500");
-                            number.classList.add("text-green-500");
-                            number.classList.remove("marker:text-red-500");
-                            number.classList.add("marker:text-green-500");
-                            i++;
-                        }
-                        else{
-                            number.classList.remove("text-green-500");
-                            number.classList.add("text-red-500");
-                            number.classList.remove("marker:text-green-500");
-                            number.classList.add("marker:text-red-500");
-                            i--;
-                        }
-
-                        // third condition
-                        if(/[a-zA-Z]/.test(password)){
-                            letter.classList.remove("text-red-500");
-                            letter.classList.add("text-green-500");
-                            letter.classList.remove("marker:text-red-500");
-                            letter.classList.add("marker:text-green-500");
-                            i++;
-                        }
-                        else{
-                            letter.classList.remove("text-green-500");
-                            letter.classList.add("text-red-500");
-                            letter.classList.remove("marker:text-green-500");
-                            letter.classList.add("marker:text-red-500");
-                        }
-
-
-                        // fourth condition
-                        if(/[!@#$%^&*¨(),.?":{}=|<>;:_+-]/.test(password)){
-                            special.classList.remove("text-red-500");
-                            special.classList.add("text-green-500");
-                            special.classList.remove("marker:text-red-500");
-                            special.classList.add("marker:text-green-500");
-                            i++;
-                        }
-                        else{
-                            special.classList.remove("text-green-500");
-                            special.classList.add("text-red-500");
-                            special.classList.remove("marker:text-green-500");
-                            special.classList.add("marker:text-red-500");
-                            i--;
-                        }
-                        
-                        if(i==4){
-                            validate = true;
-
-                        }
-                        else{
-                            validate = false;
-                        }
-                        
-                        return validate;
-                    }
-                </script>
+                <script src='./JS/script.js'></script>
+                <script></script>
                     
                 
             </form>
