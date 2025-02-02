fa87f88387f60ff3539c597d2d0648b4e277b752
ruanllima <ruanlimafn@gmail.com>
2025-02-01 23:28:35 -0300

    Sessão 'Formação Acadêmica' - Estruturação, adição das imagens e toda a dinâmica de visualização dos certificados

---
+++ src/JS/view.js
@@ -0,0 +1,15 @@
+function view(id){
+    const element = document.getElementById("session3");
+
+    if (document.getElementById(id).style.display == "block"){
+        document.getElementById(id).style.display = "none";
+        document.body.style.overflow = 'auto';
+    }
+    else{
+        document.getElementById(id).style.display = "block";
+        element.scrollIntoView({ behavior: 'smooth', block: 'start' }); // Rola até o elemento e o alinha no topo
+        document.body.style.overflow = 'hidden';
+        
+        
+    }
+}
