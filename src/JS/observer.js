14b8e3e2d09fb5aca0998d992d6814968eae4534
ruanllima <ruanlimafn@gmail.com>
2025-02-12 16:56:54 -0300

    Update landing page files

--- src/JS/observer.js
+++ src/JS/observer.js
@@ -1,50 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
-
     let lastScrollY = window.scrollY;
 
     const observer_img = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
-            const currentScrollY = window.scrollY;
             if (entry.isIntersecting) {
-                if (currentScrollY > lastScrollY) { // Verificando se o scroll está descendo
-                    entry.target.classList.remove("animate-fade-out");
-                    entry.target.classList.add("animate-fade-in-y-down");
-                    console.log("Scroll descendo: entrou");
-                } else { // Se o scroll está subindo
-                    entry.target.classList.remove("animate-fade-out");
-                    entry.target.classList.add("animate-fade-in-y-up");
-                    console.log("Scroll subindo: entrou");
-                }
+                entry.target.classList.add("animate-fade-in-y-down");
                 entry.target.classList.remove("opacity-0");
             }
-            else{
-                entry.target.classList.remove("animate-fade-in-y-down", "animate-fade-in-y-up");
-                entry.target.classList.add("animate-fade-out");
-                entry.target.classList.add("opacity-0");
-            }
-            lastScrollY = currentScrollY; // Atualiza a posição do scroll
         });
-    }, { threshold: 0.3 });
+    }, { threshold: 0.5 });
 
     const observer_img_i = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             const currentScrollY = window.scrollY;
             if (entry.isIntersecting) {
-                if (currentScrollY > lastScrollY) {
-                    entry.target.classList.remove("animate-fade-out");
-                    entry.target.classList.add("animate-fade-in-y-i-down");
-                    console.log("Scroll descendo: entrou");
-                } else {
-                    entry.target.classList.remove("animate-fade-out");
-                    entry.target.classList.add("animate-fade-in-y-i-up");
-                    console.log("Scroll subindo: entrou");
-                }
+                entry.target.classList.add("animate-fade-in-y-down");
                 entry.target.classList.remove("opacity-0");
-            }
-            else{
-                entry.target.classList.remove("animate-fade-in-y-i-down", "animate-fade-in-y-i-up");
-                entry.target.classList.add("animate-fade-out");
-                entry.target.classList.add("opacity-0");
             }
         });
     }, { threshold: 0.3 });
@@ -52,33 +23,17 @@ document.addEventListener("DOMContentLoaded", function () {
     const observer_txt = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                entry.target.classList.remove("animate-fade-out");
                 entry.target.classList.add("animate-fade-in-x");
-                console.log("entrou");
                 entry.target.classList.remove("opacity-0");
             }
-            else{
-                console.log("saiu");
-                entry.target.classList.remove("animate-fade-in-x");
-                entry.target.classList.add("animate-fade-out");
-                entry.target.classList.add("opacity-0");
-            }
         });
-    }, { threshold: 0.4 });
+    }, { threshold: 0.5 });
 
     const observer_txt_i = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                entry.target.classList.remove("animate-fade-out");
-                entry.target.classList.add("animate-fade-in-x-i");
-                console.log("entrou");
+                entry.target.classList.add("animate-fade-in-x");
                 entry.target.classList.remove("opacity-0");
-            }
-            else{
-                console.log("saiu");
-                entry.target.classList.remove("animate-fade-in-x-i");
-                entry.target.classList.add("animate-fade-out");
-                entry.target.classList.add("opacity-0");
             }
         });
     }, { threshold: 0.4 });
@@ -86,9 +41,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const observer_intro1 = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                entry.target.classList.remove("animate-fade-out");
-                entry.target.classList.add("animate-fade-in-x-i");
-                console.log("entrou");
+                entry.target.classList.add("animate-fade-in-y");
                 entry.target.classList.remove("opacity-0");
             }
             
@@ -99,8 +52,7 @@ document.addEventListener("DOMContentLoaded", function () {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 setTimeout(() => {
-                    entry.target.classList.remove("animate-fade-out");
-                    entry.target.classList.add("animate-fade-in-x");
+                    entry.target.classList.add("animate-fade-in-y");
                     entry.target.classList.remove("opacity-0");
                 }, 1000)
             
@@ -120,6 +72,68 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }, { threshold: 0.2 });
 
+    const observer_cursos = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("animate-fade-in-y");
+                console.log("entrou");
+                entry.target.classList.remove("opacity-0");
+            }
+        });
+    }, { threshold: 0.3 });
+
+    const observer_title = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("animate-fade-in-fast");
+                console.log("entrou");
+                entry.target.classList.remove("opacity-0");
+            }
+        });
+    }, { threshold: 0.4 });
+
+    const observer_init1 = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("animate-fade-fall");
+                entry.target.classList.remove("opacity-0");
+            }
+        });
+    }, { threshold: 0.4 });
+
+    const observer_init2 = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+            setTimeout(() => {
+                entry.target.classList.add("animate-fade-fall");
+                entry.target.classList.remove("opacity-0");
+            }, 1100)
+            }
+        });
+    }, { threshold: 0.4 });
+
+    const observer_init3 = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+            setTimeout(() => {
+                entry.target.classList.add("animate-fade-fall");
+                entry.target.classList.remove("opacity-0");
+            }, 1400)
+            }
+        });
+    }, { threshold: 0.4 });
+
+    const observer_fadeiny = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("animate-fade-in-y");
+                console.log("entrou");
+                entry.target.classList.remove("opacity-0");
+            }
+        });
+    }, { threshold: 0.3 });
+    
+
     document.querySelectorAll('[id="img"]').forEach(el => observer_img.observe(el));
     document.querySelectorAll('[id="txt"]').forEach(el => observer_txt.observe(el));
     document.querySelectorAll('[id="img-i"]').forEach(el => observer_img_i.observe(el));
@@ -127,5 +141,15 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll('[id="intro1"]').forEach(el => observer_intro1.observe(el));
     document.querySelectorAll('[id="intro2"]').forEach(el => observer_intro2.observe(el));
     document.querySelectorAll('[id="intro3"]').forEach(el => observer_intro3.observe(el));
+    document.querySelectorAll('[id="curso1"]').forEach(el => observer_cursos.observe(el));
+    document.querySelectorAll('[id="curso2"]').forEach(el => observer_cursos.observe(el));
+    document.querySelectorAll('[id="curso3"]').forEach(el => observer_cursos.observe(el));
+    document.querySelectorAll('[id="curso4"]').forEach(el => observer_cursos.observe(el));
+    document.querySelectorAll('[id="title"]').forEach(el => observer_title.observe(el));
+    document.querySelectorAll('[id="init1"]').forEach(el => observer_init1.observe(el));
+    document.querySelectorAll('[id="init2"]').forEach(el => observer_init2.observe(el));
+    document.querySelectorAll('[id="init3"]').forEach(el => observer_init3.observe(el));
+    document.querySelectorAll('#data_projects > *').forEach(el => observer_fadeiny.observe(el));
+
     
 });
--- src/JS/view.js
+++ src/JS/view.js
@@ -1,14 +1,82 @@
-function view(id){
-    const element = document.getElementById("session3");
+function view(id, fixed){
+    const element = document.getElementById(fixed);
 
-    if (document.getElementById(id).style.display == "block"){
+    if (document.getElementById(id).style.display == ""){
         document.getElementById(id).style.display = "none";
         document.body.style.overflow = 'auto';
     }
     else{
-        document.getElementById(id).style.display = "block";
+        document.getElementById(id).style.display = "";
         element.scrollIntoView({ behavior: 'smooth', block: 'start' }); // Rola até o elemento e o alinha no topo
         document.body.style.overflow = 'hidden';
       
     }
 }
+
+function view_tab(tab1, tab2, tab3, local){
+    const element = document.getElementById(local);
+    console.log(element);
+    if (document.getElementById(tab1).style.display == ""){
+        element.scrollIntoView({ behavior: 'smooth', block: 'start' }); // Rola até o elemento e o alinha no topo
+        document.body.style.overflow = 'hidden';
+
+
+        if (document.getElementById(tab2).style.display == ""){
+            document.getElementById(tab2).style.display = "none";  
+            console.log('teste3'); 
+            document.getElementById(tab3).style.display = "";
+            element.scrollIntoView({ behavior: 'smooth', block: 'start' }); // Rola até o elemento e o alinha no topo
+            document.body.style.overflow = 'hidden';
+        }
+        
+        else{
+            document.getElementById(tab1).style.display = "none";
+            console.log('teste4');
+            document.body.style.overflow = 'auto';
+        }
+       
+    }
+    else{
+        console.log('teste5');
+        document.getElementById(tab1).style.display = "";
+        element.scrollIntoView({ behavior: 'smooth', block: 'start' }); // Rola até o elemento e o alinha no topo
+        document.body.style.overflow = 'hidden';
+      
+    }
+}
+
+
+
+function view_certificate(id) {
+
+    const div = document.getElementById("certificados");
+    const view = document.getElementById('view');
+    const selected = document.getElementById(id);
+    
+    document.getElementById('certificados_list').style.display = "none";
+    document.getElementById('view_container').style.display = ""; // ou ""
+
+    view.src = selected.src;
+
+    return id;
+}
+
+function change_certificate(id){
+    const div = document.getElementById("certificados_list");
+    const certificados = div.querySelectorAll('[id]');
+    const array_cert = Array.from(certificados).map(cert => cert.id);
+    const currentSrc = document.getElementById("view").src;
+    const currentCert = array_cert.find(cert => document.getElementById(cert).src === currentSrc);
+    const index = array_cert.indexOf(currentCert);
+    
+
+    if(id === 'left' && index > 0){
+        view_certificate(array_cert[index - 1]);
+    }
+
+    if(id === 'right' && index < array_cert.length - 1){
+        view_certificate(array_cert[index + 1]);
+    }
+
+}
+
